7ee5088ce476cd71f01c3bcc9f2f9eeb51c395b4
dsom08 <libgenmobile@gmail.com>
2021-02-19 14:46:32 +0900

    relocate custom mapping file

--- metro.config.js
+++ metro.config.js
@@ -9,7 +9,7 @@ const MetroConfig = require('@ui-kitten/metro-config');
 
 const evaConfig = {
   evaPackage: '@eva-design/eva',
-  customMappingPath: './src/mapping.json'
+  customMappingPath: './mapping.json'
   // Optional, but may be useful when using mapping customization feature.
   // customMappingPath: './custom-mapping.json',
 };
